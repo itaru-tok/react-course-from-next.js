ca3cd56051ebb31f053f2d0098d9e607b4d68cdc
itaruby_on_rails <itokuda@cuon.co.jp>
2023-11-12 14:58:41 +0900

    create 3 components using props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Todo from './components/Todo'
+import Backdrop from './components/Backdrop'
+import Modal from './components/Modal'
+
 function App() {
-  return <div>Hello, world!</div>
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Explore the full React course" />
+      <Backdrop />
+      <Modal />
+    </div>
+  )
 }
 
 export default App
